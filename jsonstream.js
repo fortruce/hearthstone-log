60b67e9e2307697da5641ef4f547968ee270e9c2
fortruce <>
2015-06-15 16:57:55 -0400

    writable stream for concating objects into an array before calling a callback with the result

---
+++ jsonstream.js
@@ -0,0 +1,21 @@
+// JsonStream is a writable stream that collects multiple json object chunks
+// and concats them into a single array of json objects, calling the callback
+// on the result.
+function JsonStream(cb) {
+  // don't require the use of the new keyword for consumers
+  if (!(this instanceof JsonStream))
+    return new JsonStream(cb);
+
+  Writable.call(this, {objectMode: true});
+  this.body = [];
+
+  this.on('finish', function () { cb(this.body); });
+}
+util.inherits(JsonStream, Writable);
+
+JsonStream.prototype._write = function (chunk, enc, next) {
+  this.body.push(chunk);
+  next();
+};
+
+module.exports = JsonStream;
