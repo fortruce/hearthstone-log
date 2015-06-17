933cafa4c124710d1bcfd95dc8c40536eea93189
fortruce <>
2015-06-17 16:57:31 -0400

    add basic id= parsing switching on func

--- parsers/zone.js
+++ parsers/zone.js
@@ -1,35 +1,78 @@
 var tokenize = require('./tokenize');
 var taxonomize = require('./taxonomize');
-var parser = require('./parser');
+var utils = require('./utils');
 var assign = require('object-assign');
 var assert = require('assert');
 var debug = require('debug')('Parse.Zone');
 
+//constants
+var TAXONOMIES = require('../constants').TAXONOMIES.ZONE;
+var LOG = require('../constants').LOG.ZONE;
+
+function parseZone(tokens) {
+  switch(tokens[0]) {
+  case '':
+    tokens.shift();
+    return {};
+  case 'FRIENDLY':
+  case 'OPPOSING':
+    var o = {
+      field: tokens.shift(),
+      zone: tokens.shift(),
+    }
+    if (tokens[0] === '(') {
+      // card type qualifier is present
+      assert(tokens.shift() === '(');
+      var cardType = utils.parseValue(tokens);
+      assert(['Hero', 'Hero Power', 'Weapon'].indexOf(cardType) !== -1);
+      assert(tokens.shift() === ')');
+      o.cardType = cardType;
+    }
+    return o;
+  default:
+    var n = tokens.shift();
+    assert(!isNaN(n));
+    return {index: n}
+  }
+}
+
 function parseTransition(tokens) {
   assert(tokens.shift() === 'TRANSITIONING');
   assert(tokens.shift() === 'card');
-  var o = parser.parseTagArray(tokens);
+  var o = utils.parseTagArray(tokens);
   assert(tokens.shift() === 'to');
   if (tokens[0]) {
     // field and zone qualifier present
-    var field = tokens.shift();
-    assert(['FRIENDLY', 'OPPOSING'].indexOf(field) !== -1);
-    o.field = field;
-
-    var destZone = tokens.shift();
-    assert(['DECK', 'HAND', 'GRAVEYARD', 'PLAY', 'SECRET'].indexOf(destZone) !== -1);
-    o.destZone = destZone;
-
-    if (tokens[0]) {
-      // card type qualifier present
-      assert(tokens.shift() === '(');
-      var cardType = parser.parseValue(tokens);
-      assert(['Hero', 'Hero Power', 'Weapon'].indexOf(cardType) !== -1);
-      o.cardType = cardType;
-      assert(tokens.shift() === ')');
-    }
+    o.to = parseZone(tokens);
   }
   return o;
+}
+
+function parseZoneChange(tokens) {
+  var o = Object.create(null);
+  assign(o, utils.parseKeyValuePair(tokens));
+  assign(o, utils.parseKeyValuePair(tokens));
+
+  var tags = utils.parseTagArray(tokens);
+  o.tags = tags;
+
+  var type = tokens.shift();
+  assert(['zone', 'pos'].indexOf(type) !== -1);
+  o.type = type.toUpperCase();
+
+  assert(tokens.shift() === 'from');
+  o.from = parseZone(tokens);
+  assert(tokens.shift() === '->');
+  o.to = parseZone(tokens);
+  return o;
+}
+
+function parseLocalChange(chunk) {
+  return {};
+}
+
+function parseFinishChange(chunk) {
+  return {};
 }
 
 module.exports = function parse(chunk, classify) {
@@ -39,10 +82,34 @@ module.exports = function parse(chunk, classify) {
   var result;
 
   switch(tokens[0]) {
-  case 'TRANSITIONING':
+  case LOG.TRANSITIONING:
     result = parseTransition(tokens);
     taxonomy = 'TRANSITIONING';
     break;
+
+  case LOG.ID:
+    switch(true) {
+    case !!chunk.func.match(/ProcessChanges/):
+      result = parseZoneChange(tokens);
+      taxonomy = TAXONOMIES.ZONE_CHANGE;
+      break;
+
+    case !!chunk.func.match(/LocalChangesFromTrigger/):
+      result = parseLocalChange(tokens);
+      taxonomy = TAXONOMIES.LOCAL_CHANGE;
+      break;
+
+    case !!chunk.func.match(/Finish/):
+      result = parseFinishChange(tokens);
+      taxonomy = TAXONOMIES.FINISH_CHANGE;
+      break;
+
+    default:
+      debug('no id parser found:', chunk.func, chunk.raw);
+      return undefined;
+    }
+    break;
+
   default:
     debug('no parser found:', chunk.raw);
     return undefined;
