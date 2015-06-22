4e5bc106047c70c29b9874ae5a59d40af5b13b04
fortruce <>
2015-06-22 16:42:55 -0400

    add extra constants

--- constants.js
+++ constants.js
@@ -12,9 +12,7 @@ module.exports = {
     }),
     ZONE: keymirror({
       TRANSITIONING: null,
-      ZONE_CHANGE: null,
-      LOCAL_CHANGE: null,
-      FINISH_CHANGE: null
+      ZONE_CHANGE: null
     })
   },
   KEYWORDS: {
@@ -24,14 +22,22 @@ module.exports = {
       GAME_ENTITY: 'GameEntity',
       PLAYER: 'Player',
       FULL_ENTITY: 'FULL_ENTITY',
-      CREATE_GAME: 'CREATE_GAME'
+      CREATE_GAME: 'CREATE_GAME',
+      M_CURRENT_TASK_LIST: 'm_currentTaskList',
+      COUNT: 'Count'
     },
     ZONE: {
       TRANSITIONING: 'TRANSITIONING',
       ID: 'id',
       PROCESS_CHANGES: /ProcessChanges/,
       LOCAL_CHANGES: /LocalChangesFromTrigger/,
-      FINISH: /Finish/
+      FINISH: /Finish/,
+      M_ID: 'm_id',
+      TASK_LIST_ID: 'taskListId',
+      SRC_ZONE: 'srcZone',
+      SRC_POS: 'srcPos',
+      CHANGE_LIST_ID: 'changeListId',
+      BRACKET_ID: '['
     }
   },
   LOGS: keymirror({
